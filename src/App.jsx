c59c036ed1ebce85d1e41c12e39ea143b2b6b8a5
Spaf <spafiii@gmail.com>
2023-11-18 00:34:43 +0200

    first commit with basic structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'primereact/resources/themes/mdc-dark-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import { PrimeReactProvider } from "primereact/api";
+
+import "primereact/resources/themes/arya-green/theme.css";
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+export default function App() {
+    return <PrimeReactProvider>
+        <></>
+    </PrimeReactProvider>
+}
